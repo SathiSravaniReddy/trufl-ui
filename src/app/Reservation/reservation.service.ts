bc7682d3ca40409eb8575f1ac67cbbfb79686451
Deepika Chaudhari <deepika.chaudhari@ptgindia.com>
2018-01-11 14:50:05 +0530

    Login Logo drag disable,Startservice padding changed

--- src/app/Reservation/reservation.service.ts
+++ src/app/Reservation/reservation.service.ts
@@ -6,19 +6,10 @@ import { constant } from '../shared/appsettings';
 @Injectable()
 export class ReservationService {
     constructor(private http: Http) {
-
     }
-
     sendreservationdetails(obj: any) {
-
-
         return this.http.post(constant.truflAPI + constant.truflBase+'Hostess/SaveRestaurantGuest', obj).map(
             (res) => res.json()
         )
-
     }
-
-
-
-
 }
