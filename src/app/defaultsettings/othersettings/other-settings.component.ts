3ee90877d2d4faa6f39fad0616a0d0ec9344d03e
rajiv.karanam <rajiv.karanam@ptgindia.com>
2018-06-14 09:58:45 +0000

    fixed the issue of Geofence

--- src/app/defaultsettings/othersettings/other-settings.component.ts
+++ src/app/defaultsettings/othersettings/other-settings.component.ts
@@ -89,10 +89,7 @@ export class OtherSettingsComponent implements OnInit {
   getOtherSelectionsDetails(restarauntid) {
     var that = this;
     this._otherservice.getOtherSettingsDetails(restarauntid).subscribe((res: any) => {
-        this.getothersettingsinfo = res._Data;
-      if (this.getothersettingsinfo[0].RestaurantNotificationMsg == null || this.getothersettingsinfo[0].RestaurantNotificationMsg == undefined) {
-         this.getothersettingsinfo[0].RestaurantNotificationMsg = 'text message';
-      }     
+        this.getothersettingsinfo = res._Data;    
       this.getothersettingsinfo.map(function (item) {
         let otherinfo = item;
         that._otherservice.setDiningExperience(otherinfo.DiningTime);
