88fd38870e45bd8fb61c8ca2f155d2508277aa0d
Ankit DIxit (People Tech Group) <v-andix@microsoft.com>
2018-07-26 16:26:10 +0530

    changes done to server tab

--- src/app/defaultsettings/manageservers/manage-servers.service.ts
+++ src/app/defaultsettings/manageservers/manage-servers.service.ts
@@ -7,87 +7,33 @@ import {Observable} from "rxjs";
 
 @Injectable()
 export class ManageServersService {
-  public RestaurentId: any
+  private results: any;
 
   constructor(private http: Http) {
-
   }
 
-
-  //service for getting all the alavilabel servers
-  GetStaffAssignTables(restaurantid) {
-    return this.http.get(constant.truflAPI + constant.truflBase + 'WaitListUser/GetStaffAssignTables/' + restaurantid).map(
-      (res) => res.json()
-    ).catch(this.handleError)
-  }
-
-  //service for posting the selected servers
-  SaveTableAssignedToStaff(table_info: any) {
-    return this.http.post(constant.truflAPI + constant.truflBase + 'WaitListUser/SaveTableAssignedToStaff', table_info).map(
-      (res) => res.json()
-    ).catch(this.handleError)
-  }
-
-
-  //service for getting all the alavilabel servers
-  getStaffDetails(restaurantid) {
-
-    return this.http.get(constant.truflAPI + constant.truflBase + 'WaitListUser/GetRestaurantSelectStaff/' + restaurantid).map(
-      (res) => res.json()
-    ).catch(this.handleError)
-  }
-
-  //service for posting the selected servers
-  postStaffDetails(staff_info: any) {
-    return this.http.post(constant.truflAPI + constant.truflBase + 'WaitListUser/SaveManageServer', staff_info).map(
-      (res) => res.json()
-    ).catch(this.handleError)
-  }
-
-  //assigning color randomly for alloated sercers
-  assignServercolor(colorCodes, RestaurantID) {
-    return this.http.get(constant.truflAPI + constant.truflBase + 'WaitListUser/AssignColorsToServer/' + colorCodes + '/' + RestaurantID).map(
-      (res) => res.json()
-    ).catch(this.handleError)
-  }
-
-  SaveSelectedStaff(RestaurantID, TruflUserID) {
-    return this.http.post(constant.truflAPI + constant.truflBase + '/WaitListUser/SaveSelectStaff/' + RestaurantID + '/' + TruflUserID + '', RestaurantID, TruflUserID).map(
-      (res) => res.json()
-    ).catch(this.handleError);
-  }
-  /* verifylogin service */
-
-
-  VerifyLogin(restaurantid) {
-    return this.http.get(constant.truflAPI + constant.truflBase + 'WaitListUser/GetRestaurantOpenDate/' + restaurantid).map(
-      (res) => res.json()
-    ).catch(this.handleError);
-
-  }
+//service for getting managingservers details
   getManageServersDetails(restarauntid) {
 
     return this.http.get(constant.truflAPI + constant.truflBase + 'WaitListUser/GetRestaurantStaffTables/' + restarauntid).map(
       (res) => res.json()).catch(this.handleError);
   }
 
-  //service for posting the updated data
+//service for posting the updated data
   postManageserverDetails(seatedinfo: any) {
     return this.http.post(constant.truflAPI + constant.truflBase + 'WaitListUser/SaveManageServer', seatedinfo).map(
       (res) => res.json()
     ).catch(this.handleError);
   }
 
-  //service for posting the modal pop up data
+//service for posting the modal pop up data
   postManageserverModalDetails(restarauntid: any, currentuserid: any, newuserid: any) {
     return this.http.post(constant.truflAPI + constant.truflBase + 'WaitListUser/UpdateServerClockOut/' + restarauntid + '/' + currentuserid + '/' + newuserid, '').map(
       (res) => res.json()
     ).catch(this.handleError);
   }
 
-  /* verifylogin service  end*/
-
-  //errror handling
+  //Handling errors
   public handleError(error: any) {
     return Observable.throw(error.status);
   }
